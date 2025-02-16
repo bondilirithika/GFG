9fe0f476c884026343a5168094f0b3c83a9fb618
Bondili Rithika Singh <168219157+bondilirithika@users.noreply.github.com>
2025-02-16 21:06:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.maxSubarraySum(arr));
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    int maxSubarraySum(int[] arr) {
+        // Your code here
+        //check if it contains all neg elements
+        int neg=0;
+        for(int i:arr)
+        {
+            if(i<0)
+            neg++;
+        }
+        int max=0;
+        if(neg==arr.length)
+        {
+            Arrays.sort(arr);
+            max=arr[arr.length-1];
+        }
+        else
+        {
+            int sum=0;
+            for(int i=0;i<arr.length;i++)
+            {
+                sum+=arr[i];
+                max=Math.max(max,sum);
+                if(sum<0)
+                sum=0;
+            }
+        }
+        return max;
+    }
+}
